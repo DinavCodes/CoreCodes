fab256f4383c32d1157b8e090132403e295e1a36
dinavahire <ahiredinav@gmail.com>
2023-01-31 07:47:49 +0100

    First commit

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="Dinav",Email="dev1@gmail.com",Department="CSE"},
+                new Employee(){Id=2,Name="Jack",Email="dev2@gmail.com",Department="MECH"},
+                new Employee(){Id=3,Name="Jill",Email="dev3@gmail.com",Department="CIVIL"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
